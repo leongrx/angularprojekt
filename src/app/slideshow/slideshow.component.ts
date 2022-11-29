58506867167a0334401b72997edfd89e8e7b6f08
Leon Groschek <leon.groschek12@gmail.com>
2022-11-29 18:04:13 +0100

    started header menu

---
+++ src/app/slideshow/slideshow.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-slideshow',
+  templateUrl: './slideshow.component.html',
+  styleUrls: ['./slideshow.component.scss']
+})
+export class SlideshowComponent {
+  images = ['laptop.jpg', 'macbook.jpg', 'turm.jpg'];
+  headlines = ['Bring engineering to the next level',
+  'Born to code',
+  'Graduated at TUM'
+  ];
+  currentImage = 0;
+  showImage = true;
+
+  ngOnInit() {
+    this.updateImage();
+  }
+
+  updateImage() {
+    setInterval(() => {
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.images.length;
+      this.showImage = false;
+
+      setTimeout(() => {
+        this.showImage = true;
+      }, 10);
+    }, 8000)
+  }
+}
